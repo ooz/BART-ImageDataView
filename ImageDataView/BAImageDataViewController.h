e2347d5dc404ad4e6a69bb95af5601ceaf126423
ooz <oliverzscheyge@gmail.com>
2012-06-21 14:50:26 +0200

    Add EDNA and ImageDataView files

---
+++ ImageDataView/BAImageDataViewController.h
@@ -0,0 +1,13 @@
+//
+//  BAImageData.h
+//  ImageDataView
+//
+//  Created by Oliver Zscheyge on 6/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface BAImageData : NSViewController
+
+@end
---
+++ ImageDataView/BAImageDataViewController.m
@@ -0,0 +1,27 @@
+//
+//  BAImageData.m
+//  ImageDataView
+//
+//  Created by Oliver Zscheyge on 6/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "BAImageData.h"
+
+@interface BAImageData ()
+
+@end
+
+@implementation BAImageData
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
