cf3d1cfa7d67ec44537c8d537f949f787f80e34e
ooz <oliverzscheyge@gmail.com>
2012-07-06 15:53:52 +0200

    Add custom NSImageView class

---
+++ ImageDataView/BABrainImageView.h
@@ -0,0 +1,13 @@
+//
+//  BABrainImageView.h
+//  ImageDataView
+//
+//  Created by Oliver Zscheyge on 7/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface BABrainImageView : NSImageView
+
+@end
---
+++ ImageDataView/BABrainImageView.m
@@ -0,0 +1,28 @@
+//
+//  BABrainImageView.m
+//  ImageDataView
+//
+//  Created by Oliver Zscheyge on 7/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "BABrainImageView.h"
+
+@implementation BABrainImageView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    [super drawRect:dirtyRect];
+}
+
+@end
