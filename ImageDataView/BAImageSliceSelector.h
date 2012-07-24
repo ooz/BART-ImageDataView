8819550578d07d1482a5e64f0ab3a00addbe6c90
ooz <oliverzscheyge@gmail.com>
2012-07-24 13:34:44 +0200

    Add methods to find relevant dimensions/size

--- ImageDataView/BAImageSliceSelector.h
+++ ImageDataView/BAImageSliceSelector.h
@@ -9,7 +9,26 @@
 #import <Foundation/Foundation.h>
 #import "EDDataElement.h"
 
+enum ImageDimension {
+      DIM_WIDTH
+    , DIM_HEIGHT
+    , DIM_SLICE
+//    , DIM_TIMESTEP
+};
+
 @interface BAImageSliceSelector : NSObject
+
+/**
+ * Returns a 3-dimensional array of original image dimensions
+ * corresponding to (DIM_WIDTH, DIM_HEIGHT, DIM_SLICE) in the target image space.
+ * 
+ * E.g. a sagittal image that should be aligned to axial orientation would return
+ * an array containing (DIM_SLICE, DIM_WIDTH, DIM_HEIGHT).
+ *
+ * Memory management: callers needs to free the array!
+ */
+-(enum ImageDimension*)getDimensionsFrom:(EDDataElement*)image
+                               alignedTo:(enum ImageOrientation)orientation;
 
 -(NSArray*)select:(size_t)n 
        slicesFrom:(EDDataElement*)image
--- ImageDataView/BAImageSliceSelector.m
+++ ImageDataView/BAImageSliceSelector.m
@@ -8,13 +8,111 @@
 
 #import "BAImageSliceSelector.h"
 
+const size_t RELEVANT_DIMENSIONS = 3;
+const size_t SLICE_DIMENSION_INDEX = 2;
+
+
+@interface BAImageSliceSelector (__privateMethods__)
+
+-(size_t)getRelevantSize:(EDDataElement*)image
+               alignedTo:(enum ImageOrientation)orientation;
+
+@end
+
+
 @implementation BAImageSliceSelector
+
+-(size_t)getRelevantSize:(EDDataElement*)image
+               alignedTo:(enum ImageOrientation)orientation
+{
+    BARTImageSize* imageSize = [image getImageSize]; 
+    
+    enum ImageDimension* dims = [self getDimensionsFrom:image alignedTo:orientation];
+    enum ImageDimension sliceDim = dims[SLICE_DIMENSION_INDEX];
+    free(dims);
+    
+    size_t relevantSize = 0;
+    switch (sliceDim) {
+        case DIM_WIDTH:
+            relevantSize = imageSize.columns;
+            break;
+        case DIM_HEIGHT:
+            relevantSize = imageSize.rows;
+        case DIM_SLICE:
+            relevantSize = imageSize.slices;
+        default:
+            NSLog(@"Error: could not find relevant slice dimension! Defaulting to 0!");
+            break;
+    }
+    
+    return relevantSize;
+}
+
+-(enum ImageDimension*)getDimensionsFrom:(EDDataElement*)image
+                               alignedTo:(enum ImageOrientation)orientation
+{
+    enum ImageOrientation mainOrientation = [image getMainOrientation];
+    BOOL isSagittal = mainOrientation == ORIENT_SAGITTAL || mainOrientation == ORIENT_REVSAGITTAL;
+    BOOL isAxial    = mainOrientation == ORIENT_AXIAL    || mainOrientation == ORIENT_REVAXIAL;
+    BOOL isCoronal  = mainOrientation == ORIENT_CORONAL  || mainOrientation == ORIENT_REVCORONAL;
+    
+    BOOL targetSagittal = orientation == ORIENT_SAGITTAL || orientation == ORIENT_REVSAGITTAL;
+    BOOL targetAxial    = orientation == ORIENT_AXIAL    || orientation == ORIENT_REVAXIAL;
+    BOOL targetCoronal  = orientation == ORIENT_CORONAL  || orientation == ORIENT_REVCORONAL;
+    
+    enum ImageDimension* dims = malloc(sizeof(enum ImageDimension) * RELEVANT_DIMENSIONS);
+    
+    if ((isSagittal && targetSagittal)
+        || (isAxial && targetAxial)
+        || (isCoronal && targetCoronal)) {
+        dims[0] = DIM_WIDTH;
+        dims[1] = DIM_HEIGHT;
+        dims[2] = DIM_SLICE;
+        
+    } else if (isSagittal && targetAxial) {
+        dims[0] = DIM_SLICE;
+        dims[1] = DIM_WIDTH;
+        dims[2] = DIM_HEIGHT;
+        
+    } else if ((isSagittal && targetCoronal)
+               || (isCoronal && targetSagittal)) {
+        dims[0] = DIM_SLICE;
+        dims[1] = DIM_HEIGHT;
+        dims[2] = DIM_WIDTH;
+        
+    } else if (isAxial && targetSagittal) {
+        dims[0] = DIM_HEIGHT;
+        dims[1] = DIM_SLICE;
+        dims[2] = DIM_WIDTH;
+    
+    } else if ((isAxial && targetCoronal) 
+               || (isCoronal && targetAxial)) {
+        dims[0] = DIM_WIDTH;
+        dims[1] = DIM_SLICE;
+        dims[2] = DIM_HEIGHT;
+    }
+    
+    return dims;
+}
 
 -(NSArray*)select:(size_t)n 
        slicesFrom:(EDDataElement*)image
         alignedTo:(enum ImageOrientation)orientation
 {
-    return nil;
+    size_t relevantSize = [self getRelevantSize:image alignedTo:orientation];
+    
+    size_t size;
+    if (n <= relevantSize) {
+        size = n;
+    } else {
+        size = relevantSize;
+    }
+    
+    NSArray* relevantSlices = [NSMutableArray arrayWithCapacity:size]; 
+
+    // TODO
+    
+    return relevantSlices;
 }
 
 @end
