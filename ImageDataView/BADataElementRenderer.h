a995734f7c31502836c76bf21d2d6e411d61b328
ooz <oliverzscheyge@gmail.com>
2012-08-24 11:43:00 +0200

    Add renderer class to prepare controller refactoring

---
+++ ImageDataView/BADataElementRenderer.h
@@ -0,0 +1,13 @@
+//
+//  BADataElementRenderer.h
+//  ImageDataView
+//
+//  Created by Oliver Zscheyge on 8/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BADataElementRenderer : NSObject
+
+@end
---
+++ ImageDataView/BADataElementRenderer.m
@@ -0,0 +1,13 @@
+//
+//  BADataElementRenderer.m
+//  ImageDataView
+//
+//  Created by Oliver Zscheyge on 8/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "BADataElementRenderer.h"
+
+@implementation BADataElementRenderer
+
+@end
