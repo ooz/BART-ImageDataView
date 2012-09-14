5f4ffd0941415c1abb36c3ad2787b3324b868221
ooz <oliverzscheyge@gmail.com>
2012-09-14 14:13:51 +0200

    Add image filter class

---
+++ ImageDataView/BAImageFilter.h
@@ -0,0 +1,13 @@
+//
+//  BAImageFilter.h
+//  ImageDataView
+//
+//  Created by Oliver Zscheyge on 9/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BAImageFilter : NSObject
+
+@end
---
+++ ImageDataView/BAImageFilter.m
@@ -0,0 +1,13 @@
+//
+//  BAImageFilter.m
+//  ImageDataView
+//
+//  Created by Oliver Zscheyge on 9/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "BAImageFilter.h"
+
+@implementation BAImageFilter
+
+@end
