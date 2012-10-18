b012e4839677a6dafab595aa3199e373814eb3e0
ooz <oliverzscheyge@gmail.com>
2012-10-18 16:14:44 +0200

    Comment fix

--- ImageDataView/ColorMappingFilterTwoDomains.h
+++ ImageDataView/ColorMappingFilterTwoDomains.h
@@ -3,7 +3,7 @@
 //  ImageDataView
 //
 //  Created by Oliver Zscheyge on 10/18/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 MPI CBS. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
