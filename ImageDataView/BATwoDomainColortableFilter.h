3863104956d6964a22138ef327a400d162f66717
ooz <oliverzscheyge@gmail.com>
2012-10-18 15:39:35 +0200

    Impl filter

--- ImageDataView/BATwoDomainColortableFilter.h
+++ ImageDataView/BATwoDomainColortableFilter.h
@@ -3,11 +3,14 @@
 //  ImageDataView
 //
 //  Created by Oliver Zscheyge on 10/18/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 MPI CBS. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
-@interface BATwoDomainColortableFilter : NSObject
+#import "BASingleDomainColortableFilter.h"
+
+/** Colortable filter supporting two domains (regions). */
+@interface BATwoDomainColortableFilter : BASingleDomainColortableFilter
 
 @end
