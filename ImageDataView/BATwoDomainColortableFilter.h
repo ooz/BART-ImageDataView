532f0c2ab146348441a69fc80d5fabdb331dfc59
ooz <oliverzscheyge@gmail.com>
2012-10-18 15:30:49 +0200

    Add colortable filter supporting two domains

---
+++ ImageDataView/BATwoDomainColortableFilter.h
@@ -0,0 +1,13 @@
+//
+//  BATwoDomainColortableFilter.h
+//  ImageDataView
+//
+//  Created by Oliver Zscheyge on 10/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BATwoDomainColortableFilter : NSObject
+
+@end
---
+++ ImageDataView/BATwoDomainColortableFilter.m
@@ -0,0 +1,13 @@
+//
+//  BATwoDomainColortableFilter.m
+//  ImageDataView
+//
+//  Created by Oliver Zscheyge on 10/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "BATwoDomainColortableFilter.h"
+
+@implementation BATwoDomainColortableFilter
+
+@end
