68beb48f7aee77e70316a92a2008f98d572f303b
ooz <oliverzscheyge@gmail.com>
2013-04-18 17:38:19 +0200

    ROIController implementation boilerplate

--- ImageDataView/ROI/BAROIController.m
+++ ImageDataView/ROI/BAROIController.m
@@ -10,4 +10,44 @@
 
 @implementation BAROIController
 
+- (id)initWithNibName:(NSString *)nibNameOrNil
+               bundle:(NSBundle *)nibBundleOrNil
+{
+    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
+    }
+    
+    return self;
+}
+
+-(void)awakeFromNib
+{
+
+}
+
+-(void)dealloc
+{
+    [super dealloc];
+}
+
+
+-(IBAction)setTool:(id)sender
+{
+    
+}
+
+-(IBAction)setMode:(id)sender
+{
+    
+}
+
+-(IBAction)setROI:(id)sender
+{
+    
+}
+
+-(IBAction)setThreshold:(id)sender
+{
+    
+}
+
 @end
