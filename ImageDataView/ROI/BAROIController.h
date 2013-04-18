3d9e1d538b54f25839b729b1bff325bb489a2e46
ooz <oliverzscheyge@gmail.com>
2013-04-18 16:01:23 +0200

    Add ROI controller

---
+++ ImageDataView/ROI/BAROIController.h
@@ -0,0 +1,13 @@
+//
+//  BAROIController.h
+//  ImageDataView
+//
+//  Created by Oliver Zscheyge on 4/18/13.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface BAROIController : NSController
+
+@end
---
+++ ImageDataView/ROI/BAROIController.m
@@ -0,0 +1,13 @@
+//
+//  BAROIController.m
+//  ImageDataView
+//
+//  Created by Oliver Zscheyge on 4/18/13.
+//
+//
+
+#import "BAROIController.h"
+
+@implementation BAROIController
+
+@end
