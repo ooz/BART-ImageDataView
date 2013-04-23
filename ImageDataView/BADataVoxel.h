6f2dbac1836bbc06c1acb2ee150eca3507b639ad
ooz <oliverzscheyge@gmail.com>
2013-04-23 17:01:04 +0200

    Relocate method

--- ImageDataView/BADataVoxel.h
+++ ImageDataView/BADataVoxel.h
@@ -12,14 +12,14 @@
     
 }
 
--(id)initWithColumn:(NSUInteger)c
-                row:(NSUInteger)r
-              slice:(NSUInteger)s
-           timestep:(NSUInteger)ts;
-
 @property (readonly) NSUInteger column;
 @property (readonly) NSUInteger row;
 @property (readonly) NSUInteger slice;
 @property (readonly) NSUInteger timestep;
+
+-(id)initWithColumn:(NSUInteger)c
+                row:(NSUInteger)r
+              slice:(NSUInteger)s
+           timestep:(NSUInteger)ts;
 
 @end
