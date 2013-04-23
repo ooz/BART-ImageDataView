5ba97f9562b50cbc7c97899e45133fd2311de539
ooz <oliverzscheyge@gmail.com>
2013-04-23 17:40:35 +0200

    Further documentation

--- ImageDataView/ROI/BADataClickHandling.h
+++ ImageDataView/ROI/BADataClickHandling.h
@@ -8,6 +8,17 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ * Protocol for communicating with the ROIController.
+ */
 @protocol BADataClickHandling <NSObject>
+
+/**
+ * Handler for "clicking" on a DataElement at a given 4D point.
+ *
+ * \param data EDDataElement defining that image space (main orientation and dimensions).
+ * \param p    BADataVoxel (4D point) that was clicked in the space of the parameter data.
+ */
+-(void)clickOn:(EDDataElement*)data at:(BADataVoxel)p;
 
 @end
