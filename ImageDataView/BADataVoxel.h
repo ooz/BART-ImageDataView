a15a70d3ba97ff045aaa68e1fba8421a8bd04622
ooz <oliverzscheyge@gmail.com>
2013-04-30 14:51:20 +0200

    Add orientation conversion for DataVoxel. Documentation

--- ImageDataView/BADataVoxel.h
+++ ImageDataView/BADataVoxel.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "EDDataElement.h"
 
 /** 
  * Class representing a 4D point.
@@ -20,9 +21,20 @@
 @property (readonly) NSUInteger slice;
 @property (readonly) NSUInteger timestep;
 
+/** Initializer.
+ */
 -(id)initWithColumn:(NSUInteger)c
                 row:(NSUInteger)r
               slice:(NSUInteger)s
            timestep:(NSUInteger)ts;
+
+/**
+ * Converts this voxel object (in place) from one orientation to another.
+ *
+ * \param srcOrient The ImageOrientation this voxel is assumed to be in (currently).
+ * \param tarOrient The ImageOrientation this voxel should be converted to.
+ */
+-(void)convertFrom:(enum ImageOrientation)srcOrient
+                to:(enum ImageOrientation)tarOrient;
 
 @end
--- ImageDataView/BADataVoxel.m
+++ ImageDataView/BADataVoxel.m
@@ -42,6 +42,41 @@
     return self;
 }
 
+-(void)convertFrom:(enum ImageOrientation)srcOrient
+                to:(enum ImageOrientation)tarOrient
+{
+    BOOL isSagittal = srcOrient == ORIENT_SAGITTAL || srcOrient == ORIENT_REVSAGITTAL;
+    BOOL isAxial    = srcOrient == ORIENT_AXIAL    || srcOrient == ORIENT_REVAXIAL;
+    BOOL isCoronal  = srcOrient == ORIENT_CORONAL  || srcOrient == ORIENT_REVCORONAL;
+    
+    BOOL targetSagittal = tarOrient == ORIENT_SAGITTAL || tarOrient == ORIENT_REVSAGITTAL;
+    BOOL targetAxial    = tarOrient == ORIENT_AXIAL    || tarOrient == ORIENT_REVAXIAL;
+    BOOL targetCoronal  = tarOrient == ORIENT_CORONAL  || tarOrient == ORIENT_REVCORONAL;
+    
+    NSUInteger swap = self->column;
+    if (isSagittal && targetAxial) {
+        self->column = self->slice;
+        self->slice = self->row;
+        self->row = swap;
+        
+    } else if ((isSagittal && targetCoronal)
+               || (isCoronal && targetSagittal)) {
+        self->column = self->slice;
+        self->slice = swap;
+        
+    } else if (isAxial && targetSagittal) {
+        self->column = self->row;
+        self->row = self->slice;
+        self->slice = swap;
+        
+    } else if ((isAxial && targetCoronal)
+               || (isCoronal && targetAxial)) {
+        swap = self->row;
+        self->row = self->slice;
+        self->slice = swap;
+    }
+}
+
 -(NSString*)description {
     return [NSString stringWithFormat: @"BADataVoxel(col=%ld, row=%ld, slice=%ld, ts=%ld)",
             self->column, self->row, self->slice, self->timestep];
