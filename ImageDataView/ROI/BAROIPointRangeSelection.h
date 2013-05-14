4f99479e2dc6b9edade1098e14c035064354583f
ooz <oliverzscheyge@gmail.com>
2013-05-14 15:34:59 +0200

    New ROI selection tool PointRange ('MagicCluster')

---
+++ ImageDataView/ROI/BAROIPointRangeSelection.h
@@ -0,0 +1,13 @@
+//
+//  BAROIPointRangeSelection.h
+//  ImageDataView
+//
+//  Created by Oliver Zscheyge on 5/14/13.
+//
+//
+
+#import "BAROIPointThresholdSelection.h"
+
+@interface BAROIPointRangeSelection : BAROIPointThresholdSelection
+
+@end
---
+++ ImageDataView/ROI/BAROIPointRangeSelection.m
@@ -0,0 +1,13 @@
+//
+//  BAROIPointRangeSelection.m
+//  ImageDataView
+//
+//  Created by Oliver Zscheyge on 5/14/13.
+//
+//
+
+#import "BAROIPointRangeSelection.h"
+
+@implementation BAROIPointRangeSelection
+
+@end
