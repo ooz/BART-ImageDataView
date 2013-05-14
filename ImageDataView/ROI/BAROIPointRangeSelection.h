721993f015f2e4e35eb41b2b3b06b2f200edae7a
ooz <oliverzscheyge@gmail.com>
2013-05-14 15:55:42 +0200

    Impl PointRangeSelection

--- ImageDataView/ROI/BAROIPointRangeSelection.h
+++ ImageDataView/ROI/BAROIPointRangeSelection.h
@@ -8,6 +8,24 @@
 
 #import "BAROIPointThresholdSelection.h"
 
-@interface BAROIPointRangeSelection : BAROIPointThresholdSelection
+@interface BAROIPointRangeSelection : BAROIPointThresholdSelection {
+
+    /** Upper bound of the value range to select in. */
+    float mMax;
+    
+    // Note: the lower bound property (min) is stored in the property
+    //       super->mThreshold
+    
+}
+
+@property (nonatomic, readonly) float max;
+
+/** Initializer.
+ */
+-(id)initWithReference:(EDDataElement*)data
+                 point:(BADataVoxel*)p
+                  mode:(enum ROISelectionMode)m
+               inRange:(float)min
+                   and:(float)max;
 
 @end
